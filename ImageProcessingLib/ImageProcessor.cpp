f3ff62f38c61ed735203278ecf0afd9ce7fc9f15
Andrew Paredes <andrew.paredes71@gmail.com>
2026-04-04 15:52:46 -0400

    Updated workflow file to use curl for potentially faster pulling down of opencv and some file cleanup

--- ImageProcessingLib/ImageProcessor.cpp
+++ ImageProcessingLib/ImageProcessor.cpp
@@ -9,16 +9,8 @@ extern "C" {
         cv::Mat img = cv::Mat(img_width, img_height, CV_8UC1, pixel_data).clone(); //cloning to ensure if pixel_data is destroyed at some point the mat will not become invalid
         cv::Mat result;
        
-        //cv add or cv subtract will handle clamping values such that they cannot go below 0 or 255 for 8 bit values
+        //cv add will handle clamping values such that they cannot go below 0 or 255 for 8 bit values
         cv::add(img, cv::Scalar(brightness_adjustment), result);
-     /*   if (brightness_adjustment > 0)
-        {
-            cv::add(img, cv::Scalar(brightness_adjustment), result);
-        }
-        else 
-        {
-            cv::subtract(img, cv::Scalar(brightness_adjustment), result);
-        }*/
 
         if (img.isContinuous())
         {
